8233c809644170a12193e3ddcd6b8345c0172e53
Subhikathir <subhikshaphy1821@gmail.com>
2024-02-05 23:17:26 +0530

    frnt end

--- src/components/config.js
+++ src/components/config.js
@@ -1,4 +1,4 @@
 // config.js
-const API_BASE_URL = 'https://airfinal-subhiksha.onrender.com';
+const API_BASE_URL = 'https://airline-backend-subhiksha.onrender.com';
 //const API_BASE_URL = 'http://localhost:5000';
 export default API_BASE_URL;
