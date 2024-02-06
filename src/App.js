703d1c00c67d9bcf09d55a6448e38b1af704f9da
Subhikathir <subhikshaphy1821@gmail.com>
2024-02-06 20:27:08 +0530

    second

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
     <AuthProvider>
       <Router>
         <Routes>
+        <Route path="/" element={<Homepage />} />
           <Route path="/home" element={<Homepage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/ticket-booking" element={<TicketBooking />} />
